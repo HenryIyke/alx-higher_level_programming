2d2c4ff4324f9d5b2c4c3a92ee97ab3fab3692f1
HenryIyke <realhenryiyke@gmail.com>
2021-09-09 15:40:07 +0100

    first advanced

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+    argc = len(argv)
+    ops = {
+            '+': add,
+            '-': sub,
+            '*': mul,
+            '/': div,
+        }
+    if argc != 4:
+        print('Usage: {:s} <a> <operator> <b>'.format(argv[0]))
+        exit(1)
+    a = int(arg[1])
+    b = int(arg[3])
+    op = argv[2]
+    if op not in '+-*/':
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
+    ans = ops[op](a, b)
+    print("{:d} {:s} {:d} = {:d}".format(a, op, b, ans))
+
