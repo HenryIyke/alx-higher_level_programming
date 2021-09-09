ab02820efa4a40ccd9ee8f7622baaf185aef36f5
HenryIyke <realhenryiyke@gmail.com>
2021-09-09 15:56:21 +0100

    first advanced

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv, exit
+if __name__ == '__main__':
+    import sys
     from calculator_1 import add, sub, mul, div
-    argc = len(argv)
-    ops = {
-            '+': add,
-            '-': sub,
-            '*': mul,
-            '/': div,
-        }
-    if argc != 4:
-        print('Usage: {:s} <a> <operator> <b>'.format(argv[0]))
-        exit(1)
-    a = int(arg[1])
-    b = int(arg[3])
-    op = argv[2]
-    if op not in '+-*/':
+    if len(sys.argv) == 4:
+        funcs = [('+', add), ('-', sub), ('*', mul), ('/', div)]
+        for func in funcs:
+            if sys.argv[2] == func[0]:
+                a = int(sys.argv[1])
+                b = int(sys.argv[3])
+                print('{:d} {:s} {:d} = {:d}'.format(
+                    a, func[0], b, func[1](a, b)
+                    ))
+                sys.exit()
         print('Unknown operator. Available operators: +, -, * and /')
-        exit(1)
-    ans = ops[op](a, b)
-    print("{:d} {:s} {:d} = {:d}".format(a, op, b, ans))
-
+        sys.exit(1)
+    else:
+        print('Usage: {:s} <a> <operator> <b>'.format(sys.argv[0]))
+        sys.exit(1)
